5b1993f413966dcb3ec362d6bcc4d518a262fd0a
Thomas Anton <thomasanton1224@gmail.com>
2022-09-01 15:03:33 -0400

    card removal works

--- react-app/src/components/RemoveCardModal/RemoveCard.js
+++ react-app/src/components/RemoveCardModal/RemoveCard.js
@@ -1,17 +1,33 @@
 import { useDispatch } from "react-redux"
+import { updateDeckThunk } from "../../store/decks";
+import { useHistory } from 'react-router-dom'
 
-function RemoveCard({ onClick, deck }) {
+function RemoveCard({ onClick, deck, index }) {
     const dispatch = useDispatch();
-
+    const history = useHistory();
 
     const handleRemoval = async () => {
+        deck.spellcards.splice(index, 1)
+        console.log(deck.spellcards, "-- -- -- spellcards in deck after splice -- -- --")
 
+        const payload = {
+            user_id: deck.user_id,
+            name: deck.name,
+            spellcards: deck.spellcards.join(','),
+        }
+
+        const success = await dispatch(updateDeckThunk(payload, deck.id))
+
+        if (success) {
+            history.push(`/decks/${deck.id}`)
+            onClick()
+        }
     }
 
     return (
         <div className="remove-card-container">
             <h3>Remove this card from your deck?</h3>
-            <div>Yes</div>
+            <div onClick={handleRemoval}>Yes</div>
             <div onClick={onClick}>No</div>
         </div>
     )
